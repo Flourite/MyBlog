43a254ab313b8a512d3c2587a05a104808794296
flourite <lcongyou@gmail.com>
2019-04-07 18:20:43 -0400

    Initialize the project.

---
+++ congyou/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace congyou.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
